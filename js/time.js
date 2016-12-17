20b5da8ddf5cededdb40f2ea1d18a709ce99a7e9
Khuyen Nguyen <Khuyen Nguyen>
2016-12-17 13:18:21 +0700

    update

--- js/time.js
+++ js/time.js
@@ -1,16 +1,3 @@
-var menu = $('#top-navigator');
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 200) {
-        if (!menu.hasClass('navbar-fixed-top')) {
-            menu.addClass('navbar-fixed-top', 1000, "easeOutBounce");
-        }
-    } else {
-        if (menu.hasClass('navbar-fixed-top')) {
-            menu.removeClass('navbar-fixed-top', 1000, "easeOutBounce");
-        }
-    }
-});
-
 function ShowTime() {
     var a = new Date;
     var b = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
@@ -18,10 +5,8 @@ function ShowTime() {
     var d = c[a.getDay()] + ", ";
     var e = a.getYear();
     var f = a.getDate();
-    if (e < 1900) e += 1900;
     if (f < 10) d += "0" + a.getDate() + "/" + b[a.getMonth()] + "/" + e;
     else d += "" + a.getDate() + "/" + b[a.getMonth()] + "/" + e;
-
 
     var g = a.getHours();
     if (g < 10) {
@@ -39,51 +24,13 @@ function ShowTime() {
     }
 
     if (document.getElementById("timer") != null) {
-        document.getElementById("timer").innerHTML =  d+ ", " + g + ":" + p + ":" + s + ""
-        // document.getElementById("timer").innerHTML = g + ":" + p + ":" + s + ""
+        document.getElementById("timer").innerHTML = d + ", " + g + ":" + p + ":" + s + ""
+            // document.getElementById("timer").innerHTML = g + ":" + p + ":" + s + ""
     }
+
 }
 
 $(document).ready(function() {
     ShowTime();
     setInterval("ShowTime()", 1000);
-
-    var currentSpan = 2;
-
-    $('.featured-news > .nav span').click(function() {
-        $(this).parent().find('span').removeClass('hover');
-        $(this).addClass('hover');
-        $('.featured-news-content').hide();
-        $("#featured_" + $(this).text()).fadeIn('fast');
-
-        currentSpan = parseInt($(this).text()) + 1;
-
-        if (currentSpan > 4) currentSpan = 1;
-    });
-
-    var iShow = setInterval(function() {
-        $('#nav' + currentSpan).parent().find('span').removeClass('hover');
-        $('#nav' + currentSpan).addClass('hover');
-        $('.featured-news-content').hide();
-        $("#featured_" + $('#nav' + currentSpan).text()).fadeIn('medium');
-
-        currentSpan++;
-
-        if (currentSpan > 4) currentSpan = 1;
-    }, 4000);
-
-    $('.featured-news').hover(function() {
-        clearInterval(iShow);
-    }, function() {
-        iShow = setInterval(function() {
-            $('#nav' + currentSpan).parent().find('span').removeClass('hover');
-            $('#nav' + currentSpan).addClass('hover');
-            $('.featured-news-content').hide();
-            $("#featured_" + $('#nav' + currentSpan).text()).fadeIn('medium');
-
-            currentSpan++;
-
-            if (currentSpan > 4) currentSpan = 1;
-        }, 7000);
-    });
 });
